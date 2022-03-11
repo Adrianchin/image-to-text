e3443c1f14835d742370c918b12143be4284359e
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-03-11 13:34:26 -0700

    Update with working post request for google api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ImageInput from './components/imageinput';
+import 'tachyons';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+/*Note setstate is async, remember that 
+you want to use consol log (or other stuff inside code) 
+outside the function, otehrwise it will return with async properties*/
+
+  const [input, setInput] = useState('');
+  const [imageURL, setImageURL] = useState('');
+
+  const onImageSubmit = () => {
+    let data = JSON.stringify({
+      link: input
+    });
+    console.log("This is data to be put into my post request", data)
+    console.log("On image submit", input)
+    fetch('http://localhost:3000/image', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: data
+    })
+    .then(response => response.json())
+    .then(console.log)
+  }
+
+  const onImageInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  console.log("Input is", input);
+
+  const onButtonSubmit = () => {
+    setImageURL(input);
+    onImageSubmit();
+  }
+
+  console.log("imageURL is", imageURL);
+
+return (
+  <div>
+    <p className = 'f3 center'>
+        Test for google api
+    </p>
+    <div className='center'>
+      <div 
+      className='form center pa4 br3 shadow-5'
+      >
+        <input 
+        className='f4 pa2 w-70 center' 
+        type='text' 
+        onChange={onImageInput}/>
+        <button 
+        className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+        onClick={onButtonSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          Img URL Please
+        </button>
+      </div>
     </div>
+  </div>
   );
 }
 
---
+++ src/components/imageinput/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Imageinput from './imageinput'
+
+function index() {
+  return (
+    <div className='center'>
+        <Imageinput/>
+    </div>
+  )
+}
+
+export default index
