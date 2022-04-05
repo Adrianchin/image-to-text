73f839d14d2565db98d779a51b26e32c8c191142
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-04-04 19:54:51 -0600

    Working Tokenizer!

---
+++ src/components/Tokenizer/Tokenizer.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import TokenTextTable from "./Components/TokenTextTable";
+
+function Tokenizer() {
+  //Text Input for Json.
+  const [textInput, setTextInput] = useState(null);
+  const [tokenizedText, setTokenizedText] = useState(null);
+
+  function onTextSubmit() {
+    let textForTokenizing = JSON.stringify({
+      text: textInput,
+    });
+
+    async function fetchTokenization() {
+      const response = await fetch(`http://localhost:3000/tokenizetext`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: textForTokenizing,
+      });
+      const tokenizedText = await response.json();
+      setTokenizedText(tokenizedText);
+      console.log(tokenizedText);
+    }
+    fetchTokenization();
+  }
+
+  const onTextButtonSubmit = () => {
+    onTextSubmit();
+  };
+
+  const onTextInput = (event) => {
+    setTextInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <p className="f3 center">Tokenized Text Input</p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onTextInput}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onTextButtonSubmit}
+          >
+            Tokenized Text Please
+          </button>
+        </div>
+      </div>
+      <TokenTextTable 
+        tokenizedText={tokenizedText}
+      />
+    </div>
+  );
+}
+
+export default Tokenizer;
